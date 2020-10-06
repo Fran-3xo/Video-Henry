8783a617e5d6c2c5d21f117a77143637322b2d2d
Francis Ricle <francisricleruiz@gmail.com>
2020-10-05 23:03:26 -0300

    navbar agregado, y api

--- api/src/db.js
+++ api/src/db.js
@@ -7,11 +7,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-<<<<<<< HEAD
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henryapp`, {
-=======
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/henryvideos`, {
->>>>>>> main
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
