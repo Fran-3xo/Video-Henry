1946fb23b8365d978962c2407da241b5d779d15c
franco troncoso <frantronco11@gmail.com>
2020-10-06 23:05:33 -0300

    ahora si

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from "redux";
 import {clasesReducer} from "./clases";
 import {cohorteReducer} from "./cohorte";
 import {userReducer} from "./login";
-import {combineReducers} from "redux"
+
 export const rootReducer = combineReducers({
     user: !!Object.values(JSON.parse(localStorage.getItem("user"))).length?JSON.parse(localStorage.get("user")):userReducer,
     cohorte: cohorteReducer,
